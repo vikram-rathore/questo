85b475f3fa02ab2b42c1b2a4918bc8cfb39a7743
Vikram Singh Rathore <vikram.rathore@oyorooms.com>
2018-07-30 12:13:24 +0530

    Configured app for api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  before_action :underscore_params!
+
+  protect_from_forgery with: :null_session
+  respond_to :json
+
+  private
+    def underscore_params!
+      params.deep_transform_keys!(&:underscore)
+    end
 end
