fe85e4bc4d649c16ca029a2292243dea30d62979
Vikram Singh Rathore <vikram.rathore@oyorooms.com>
2018-07-30 15:45:39 +0530

    Added validations etc in questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,12 @@
 class Question < ActiveRecord::Base
   belongs_to :user
+
+  scope :asked_by, ->(username) { where(user: User.where(username: username)) }
+
+  validates :title, presence: true, allow_blank: false
+  validates :slug, uniqueness: true, exclusion: { in: ['feed'] }
+
+  before_validation do
+    self.slug ||= "#{title.to_s.parameterize}-#{rand(36**6).to_s(36)}"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ActiveRecord::Base
 
   validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
 
+  has_many :questions, dependent: :destroy
+
   def generate_jwt
     JWT.encode({ id: id,
               exp: 60.days.from_now.to_i },
