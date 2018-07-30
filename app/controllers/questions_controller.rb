8c7e1acd35f0a4084398a674dc643cbf28705593
Vikram Singh Rathore <vikram.rathore@oyorooms.com>
2018-07-30 16:02:30 +0530

    Added CRUD actions in question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @questions = Question.all.includes(:user)
@@ -9,6 +9,45 @@ class QuestionsController < ApplicationController
     @questions_count = @questions.count
 
     @questions = @questions.order(created_at: :desc).offset(params[:offset] || 0).limit(params[:limit] || 20)
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.user = current_user
+
+    if @question.save
+      render :show
+    else
+      render json: { errors: @question.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @question = Question.find_by_slug!(params[:slug])
+  end
+
+  def update
+    @question = Question.find_by_slug!(params[:slug])
+
+    if @question.user_id == @current_user_id
+      @question.update_attributes(question_params)
+
+      render :show
+    else
+      render json: { errors: { question: ['not owned by user'] } }, status: :forbidden
+    end
+  end
+
+  def destroy
+    @question = Question.find_by_slug!(params[:slug])
+
+    if @question.user_id == @current_user_id
+      @question.destroy
+
+      render json: {}
+    else
+      render json: { errors: { question: ['not owned by user'] } }, status: :forbidden
+    end
   end
 
   private
